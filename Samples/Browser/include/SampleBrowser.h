46d8716b2d414b9e34acf7536e1206f85de9aba4
devinshine <devin.xdw@gmail.com>
2020-02-07 21:20:39 +0800

    demo(iOS): Add default sample

--- Samples/Browser/include/SampleBrowser.h
+++ Samples/Browser/include/SampleBrowser.h
@@ -792,6 +792,8 @@ namespace OgreBites
             if (startupSample && mFirstRun){
                 runSample(startupSample);
             }
+            // just for demo
+            runSampleByIndex(8);
         }
 
     protected:
